bf2c2b5a6a14d8c944098c628ca3b3de834fb03e
오민석 <minsuk6803@gmail.com>
2026-04-21 23:27:52 +0900

    init: 스프링부트 프로젝트 초기 설정

---
+++ modu/src/test/java/com/ssafy/modu/ModuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.modu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
