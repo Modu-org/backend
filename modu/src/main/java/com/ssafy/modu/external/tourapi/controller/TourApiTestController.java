af39924adb19833af123e5fb0ec265fb66fdb668
오민석 <minsuk6803@gmail.com>
2026-05-03 17:10:01 +0900

    feat: Tour API 요청 및 JSON 응답 파싱 클라이언트 구현

---
+++ modu/src/main/java/com/ssafy/modu/external/tourapi/controller/TourApiTestController.java
@@ -0,0 +1,27 @@
+package com.ssafy.modu.external.tourapi.controller;
+
+import tools.jackson.databind.JsonNode;
+import com.ssafy.modu.external.tourapi.TourApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/test/tour")
+public class TourApiTestController {
+
+    private final TourApiService tourApiService;
+
+    @GetMapping("/common/{contentId}")
+    public JsonNode common(@PathVariable String contentId) {
+        return tourApiService.getCommon(contentId);
+    }
+
+    @GetMapping("/accessibility/{contentId}")
+    public JsonNode accessibility(@PathVariable String contentId) {
+        return tourApiService.getAccessibility(contentId);
+    }
+}
