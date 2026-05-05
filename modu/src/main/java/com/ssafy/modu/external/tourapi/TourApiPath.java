9dd9b9ed1c6eb99fbb7625c400637affc252ec79
오민석 <minsuk6803@gmail.com>
2026-05-05 22:32:06 +0900

    feat : 관광지 추가 정보를 받기 위한 api 경로 추가

--- modu/src/main/java/com/ssafy/modu/external/tourapi/TourApiPath.java
+++ modu/src/main/java/com/ssafy/modu/external/tourapi/TourApiPath.java
@@ -5,6 +5,8 @@ public final class TourApiPath {
     private TourApiPath() {}
 
     public static final String DETAIL_COMMON = "/detailCommon2";
+    public static final String DETAIL_INTRO = "/detailIntro2";
+    public static final String DETAIL_INFO = "/detailInfo2";
     public static final String DETAIL_WITH_TOUR = "/detailWithTour2";
     public static final String SYNC_LIST = "/areaBasedSyncList2";
 }
