67b6877b04feac8ee3cfe9d2301b916d0d5d9ca9
오민석 <minsuk6803@gmail.com>
2026-05-05 22:39:34 +0900

    feat : 상세 정보 api 추가에 의한 intro, info 서비스 함수 추가 및 동기화에 필요한 컬럼 추가

--- modu/src/main/java/com/ssafy/modu/external/tourapi/TourApiService.java
+++ modu/src/main/java/com/ssafy/modu/external/tourapi/TourApiService.java
@@ -14,31 +14,47 @@ public class TourApiService {
     private final TourApiClient client;
 
     public JsonNode getCommon(String contentId) {
-        Map<String, String> params = defaultParams();
+        Map<String, String> params = defaultParams(1, 10);
         params.put("contentId", contentId);
-
         return client.callKor(TourApiPath.DETAIL_COMMON, params);
     }
 
-    public JsonNode getAccessibility(String contentId) {
-        Map<String, String> params = defaultParams();
+    public JsonNode getIntro(String contentId, String contentTypeId) {
+        Map<String, String> params = defaultParams(1, 10);
         params.put("contentId", contentId);
+        params.put("contentTypeId", contentTypeId);
+        return client.callKor(TourApiPath.DETAIL_INTRO, params);
+    }
 
+    public JsonNode getInfo(String contentId, String contentTypeId) {
+        Map<String, String> params = defaultParams(1, 50);
+        params.put("contentId", contentId);
+        params.put("contentTypeId", contentTypeId);
+        return client.callKor(TourApiPath.DETAIL_INFO, params);
+    }
+
+    public JsonNode getAccessibility(String contentId) {
+        Map<String, String> params = defaultParams(1, 10);
+        params.put("contentId", contentId);
         return client.callKorWith(TourApiPath.DETAIL_WITH_TOUR, params);
     }
 
     public JsonNode getSync(String modifiedTime) {
-        Map<String, String> params = defaultParams();
-        params.put("modifiedtime", modifiedTime);
-        params.put("showflag", "1");
+        return getSync(modifiedTime, 1, 10, "1");
+    }
 
+    public JsonNode getSync(String modifiedTime, int pageNo, int numOfRows, String showFlag) {
+        Map<String, String> params = defaultParams(pageNo, numOfRows);
+        params.put("modifiedtime", modifiedTime);
+        params.put("showflag", showFlag);
+        params.put("arrange", "C");
         return client.callKor(TourApiPath.SYNC_LIST, params);
     }
 
-    private Map<String, String> defaultParams() {
+    private Map<String, String> defaultParams(int pageNo, int numOfRows) {
         Map<String, String> map = new HashMap<>();
-        map.put("numOfRows", "10");
-        map.put("pageNo", "1");
+        map.put("numOfRows", String.valueOf(numOfRows));
+        map.put("pageNo", String.valueOf(pageNo));
         return map;
     }
 }
