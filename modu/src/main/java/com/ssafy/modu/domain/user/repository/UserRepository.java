58287ed27e8a25443bdf4f42f301a667421f07e9
오민석 <minsuk6803@gmail.com>
2026-05-12 22:39:56 +0900

    feat : 유저 레포지토리 생성

---
+++ modu/src/main/java/com/ssafy/modu/domain/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.ssafy.modu.domain.user.repository;
+
+import com.ssafy.modu.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUserNameAndIsDeletedFalse(String userName);
+
+    Optional<User> findByUserNameAndIsDeletedFalse(String userName);
+
+    Optional<User> findByIdAndIsDeletedFalse(Long id);
+}
