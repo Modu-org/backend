d551869b1502a9b2ec7fef6dde008bafd432d96a
오민석 <minsuk6803@gmail.com>
2026-05-13 23:22:33 +0900

    refact : jwt secret 읽게 하기 위한 처리

--- modu/src/main/java/com/ssafy/modu/global/auth/jwt/JwtProperties.java
+++ modu/src/main/java/com/ssafy/modu/global/auth/jwt/JwtProperties.java
@@ -3,9 +3,11 @@ package com.ssafy.modu.global.auth.jwt;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 
