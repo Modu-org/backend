536f150a5b55b00565c2f0b688423808c538bc34
오민석 <minsuk6803@gmail.com>
2026-05-18 23:46:05 +0900

    feat : 카테고리 대분류로 매핑

--- modu/src/main/java/com/ssafy/modu/domain/accessibility/entity/enums/AccessibilityCategory.java
+++ modu/src/main/java/com/ssafy/modu/domain/accessibility/entity/enums/AccessibilityCategory.java
@@ -2,20 +2,85 @@ package com.ssafy.modu.domain.accessibility.entity.enums;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public enum AccessibilityCategory {
 
-    PHYSICAL(0),
-    VISUAL(1),
-    HEARING(2),
-    INFANT_FAMILY(3),
-    COMMON(4),
-    ETC(5);
+    PHYSICAL(0, List.of(
+            AccessibilityType.ACCESSIBLE_PARKING,
+            AccessibilityType.PARKING,
+            AccessibilityType.PUBLIC_TRANSPORT,
+            AccessibilityType.ROUTE,
+            AccessibilityType.TICKET_OFFICE,
+            AccessibilityType.WHEELCHAIR,
+            AccessibilityType.EXIT,
+            AccessibilityType.ELEVATOR,
+            AccessibilityType.RESTROOM,
+            AccessibilityType.ACCESSIBLE_RESTROOM,
+            AccessibilityType.AUDITORIUM,
+            AccessibilityType.ROOM
+    )),
+
+    VISUAL(1, List.of(
+            AccessibilityType.ACCESSIBLE_PARKING,
+            AccessibilityType.PARKING,
+            AccessibilityType.PUBLIC_TRANSPORT,
+            AccessibilityType.ROUTE,
+            AccessibilityType.TICKET_OFFICE,
+            AccessibilityType.RESTROOM,
+            AccessibilityType.BRAILLE_BLOCK,
+            AccessibilityType.HELP_DOG,
+            AccessibilityType.GUIDE_HUMAN,
+            AccessibilityType.AUDIO_GUIDE,
+            AccessibilityType.BIG_PRINT,
+            AccessibilityType.BRAILLE_PROMOTION,
+            AccessibilityType.GUIDE_SYSTEM
+    )),
+
+    HEARING(2, List.of(
+            AccessibilityType.ACCESSIBLE_PARKING,
+            AccessibilityType.PARKING,
+            AccessibilityType.PUBLIC_TRANSPORT,
+            AccessibilityType.ROUTE,
+            AccessibilityType.TICKET_OFFICE,
+            AccessibilityType.RESTROOM,
+            AccessibilityType.GUIDE_HUMAN,
+            AccessibilityType.GUIDE_SYSTEM,
+            AccessibilityType.SIGN_GUIDE,
+            AccessibilityType.VIDEO_GUIDE,
+            AccessibilityType.ROOM
+    )),
+
+    INFANT_FAMILY(3, List.of(
+            AccessibilityType.PARKING,
+            AccessibilityType.PUBLIC_TRANSPORT,
+            AccessibilityType.ROUTE,
+            AccessibilityType.RESTROOM,
+            AccessibilityType.STROLLER,
+            AccessibilityType.LACTATION_ROOM,
+            AccessibilityType.BABY_SPARE_CHAIR,
+            AccessibilityType.KIDS_FACILITY
+    )),
+
+    COMMON(4, List.of(
+            AccessibilityType.ACCESSIBLE_PARKING,
+            AccessibilityType.PARKING,
+            AccessibilityType.PUBLIC_TRANSPORT,
+            AccessibilityType.ROUTE,
+            AccessibilityType.RESTROOM
+    )),
+
+    ETC(5, List.of(
+            AccessibilityType.ETC
+    ));
 
     private final int code;
+    private final List<AccessibilityType> searchTypes;
 
-    AccessibilityCategory(int code) {
+    AccessibilityCategory(int code, List<AccessibilityType> searchTypes) {
         this.code = code;
+        this.searchTypes = searchTypes;
     }
 
     public static AccessibilityCategory fromCode(Integer code) {
