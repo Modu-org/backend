cf944b3ee78e055433c59c71372fe827ded6c175
오민석 <minsuk6803@gmail.com>
2026-05-29 00:11:49 +0900

    feat : 노드 두개 받아서 요청 보내는 코드 구현

---
+++ modu/src/main/java/com/ssafy/modu/external/kakao/KakaoMobilityPath.java
@@ -0,0 +1,9 @@
+package com.ssafy.modu.external.kakao;
+
+public final class KakaoMobilityPath {
+
+    private KakaoMobilityPath() {
+    }
+
+    public static final String DIRECTIONS = "/v1/directions";
+}
