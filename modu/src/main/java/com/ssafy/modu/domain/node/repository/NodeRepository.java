0aa29681af0b4a6a6f4b155b2dc50b583b67178e
오민석 <minsuk6803@gmail.com>
2026-05-31 17:33:04 +0900

    refact : 엣지 생성할 때 노드들 가져올 때 attraction 정보도 같이 가져오도록 설정

--- modu/src/main/java/com/ssafy/modu/domain/node/repository/NodeRepository.java
+++ modu/src/main/java/com/ssafy/modu/domain/node/repository/NodeRepository.java
@@ -12,15 +12,18 @@ public interface NodeRepository extends JpaRepository<Node, Long> {
 
     Optional<Node> findByIdAndSchedule_Id(Long nodeId, Long scheduleId);
 
-    // 노드기반으로 관광지 정보 + 무장애 정보 조회
     @EntityGraph(attributePaths = {"attraction", "attraction.accessibilityInfos"})
     Optional<Node> findWithAttractionAccessibilityByIdAndSchedule_Id(Long nodeId, Long scheduleId);
 
-    // 스케줄의 노드 개수 세기
     long countBySchedule_Id(Long scheduleId);
 
-    // 방문 일자, 방문 순서 기준으로 정렬
     List<Node> findAllBySchedule_IdAndVisitDateOrderByVisitOrderAscIdAsc(
+            Long scheduleId,
+            LocalDate visitDate
+    );
+
+    @EntityGraph(attributePaths = {"attraction"})
+    List<Node> findWithAttractionBySchedule_IdAndVisitDate(
             Long scheduleId,
             LocalDate visitDate
     );
