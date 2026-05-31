821ca22ab4e1845496254e4fc05ac59d49cb2065
오민석 <minsuk6803@gmail.com>
2026-05-31 17:32:01 +0900

    feat : 엣지 repository 생성

---
+++ modu/src/main/java/com/ssafy/modu/domain/edge/repository/EdgeRepository.java
@@ -0,0 +1,41 @@
+package com.ssafy.modu.domain.edge.repository;
+
+import com.ssafy.modu.domain.edge.entity.Edge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface EdgeRepository extends JpaRepository<Edge, Long> {
+
+    boolean existsByScheduleIdAndFromNodeIdAndToNodeId(
+            Long scheduleId,
+            Long fromNodeId,
+            Long toNodeId
+    );
+
+    Optional<Edge> findByScheduleIdAndFromNodeIdAndToNodeId(
+            Long scheduleId,
+            Long fromNodeId,
+            Long toNodeId
+    );
+
+    List<Edge> findByScheduleId(Long scheduleId);
+
+    List<Edge> findByScheduleIdAndFromNodeIdInAndToNodeIdIn(
+            Long scheduleId,
+            Collection<Long> fromNodeIds,
+            Collection<Long> toNodeIds
+    );
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+        delete from Edge e
+        where e.fromNode.id = :nodeId
+           or e.toNode.id = :nodeId
+    """)
+    void deleteAllByNodeId(Long nodeId);
+}
