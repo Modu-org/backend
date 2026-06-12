0871d5569f5d1acad1755875cefb54000512741e
오민석 <minsuk6803@gmail.com>
2026-06-12 18:31:35 +0900

    feat : 특정 백엔드 메서드를 실행하는 처리기 구현

---
+++ modu/src/main/java/com/ssafy/modu/domain/aicommand/service/ScheduleToolExecutor.java
@@ -0,0 +1,24 @@
+package com.ssafy.modu.domain.aicommand.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.ssafy.modu.domain.aicommand.dto.tool.ToolExecutionContext;
+import com.ssafy.modu.domain.aicommand.dto.tool.ToolExecutionResult;
+import com.ssafy.modu.domain.aicommand.tool.ScheduleToolHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduleToolExecutor {
+
+    private final ScheduleToolRegistry toolRegistry;
+
+    public ToolExecutionResult execute(
+            ToolExecutionContext context,
+            String toolName,
+            JsonNode arguments
+    ) {
+        ScheduleToolHandler handler = toolRegistry.getHandler(toolName);
+        return handler.execute(context, arguments);
+    }
+}
