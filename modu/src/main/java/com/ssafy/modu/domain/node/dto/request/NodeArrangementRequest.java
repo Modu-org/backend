1815c69ef576ffea95c03e6c46559b4b12c0cbd4
오민석 <minsuk6803@gmail.com>
2026-06-12 15:32:21 +0900

    feat : 서버 내부에서 노드 배치를 하기 위한 dto 생성 메서드 구현

--- modu/src/main/java/com/ssafy/modu/domain/node/dto/request/NodeArrangementRequest.java
+++ modu/src/main/java/com/ssafy/modu/domain/node/dto/request/NodeArrangementRequest.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,24 +14,35 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NodeArrangementRequest {
 
     @NotEmpty(message = "일자별 노드 배치 정보는 비어 있을 수 없습니다.")
     @Valid
     private List<DayArrangement> days;
 
+    public static NodeArrangementRequest of(List<DayArrangement> days) {
+        return new NodeArrangementRequest(days);
+    }
+
     @Getter
     @NoArgsConstructor
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class DayArrangement {
 
         private LocalDate date; // null이면 미지정 그룹
 
         @Valid
         private List<NodeArrangement> nodes;
+
+        public static DayArrangement of(LocalDate date, List<NodeArrangement> nodes) {
+            return new DayArrangement(date, nodes);
+        }
     }
 
     @Getter
     @NoArgsConstructor
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class NodeArrangement {
 
         @NotNull(message = "노드 ID는 필수입니다.")
@@ -37,5 +50,9 @@ public class NodeArrangementRequest {
 
         @Min(value = 1, message = "방문 순서는 1 이상이어야 합니다.")
         private Integer visitOrder;
+
+        public static NodeArrangement of(Long nodeId, Integer visitOrder) {
+            return new NodeArrangement(nodeId, visitOrder);
+        }
     }
 }
