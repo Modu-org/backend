34fc9bd606026515a5378831942da391ed7949f4
오민석 <minsuk6803@gmail.com>
2026-06-17 16:45:39 +0900

    feat : 프론트에 내려줄, 음성 명령 수행의 결과 enum

---
+++ modu/src/main/java/com/ssafy/modu/domain/aicommand/dto/enums/VoiceCommandResultType.java
@@ -0,0 +1,10 @@
+package com.ssafy.modu.domain.aicommand.dto.enums;
+
+public enum VoiceCommandResultType {
+    ATTRACTION_SEARCH_RESULT,
+    ATTRACTION_DETAIL,
+    SCHEDULE_CANDIDATES,
+    SCHEDULE_UPDATED,
+    NEED_MORE_INFO,
+    SIMPLE_MESSAGE
+}
