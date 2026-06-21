eb46379b46b28400c79e98f294217a2b17ebf0a3
오민석 <minsuk6803@gmail.com>
2026-06-21 20:54:10 +0900

    feat : 여행자-보호자 관계 설정 요청 dto 구현

---
+++ modu/src/main/java/com/ssafy/modu/domain/caregiver/dto/request/CaregiverRelationRequest.java
@@ -0,0 +1,13 @@
+package com.ssafy.modu.domain.caregiver.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CaregiverRelationRequest {
+
+    @NotNull(message = "보호자 ID는 필수입니다.")
+    private Long caregiverId;
+}
